e3ac62cbd2300e03cfdb0aeebfa2993b146a4066
stardust224 <38307562+stardust224@users.noreply.github.com>
2018-04-19 05:40:23 +0900

    Add files via upload

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 
 public class Hello {
 	public static void main(String[]args){
-		System.out.println(" Hello, world. I'm programming begginer. But I want to learn programming.");
+		System.out.println(" Hello, world. I'm programming expert. But I want to learn programming more.");
 	}
 }
